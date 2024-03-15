82b891b524f767195c99bf230fb6d3b233417d16
William Knight <William.Jeffery.Knight@protonmail.com>
2024-03-15 11:52:48 -0400

    Regex can also take specific flags to alter the pattern matching behavior. Flags are added after the closing /. The g flag, which stands for "global", will tell the pattern to continue looking after it has found a match. Here is an example: const helloRegex = /hello/g; Add the g flag to your regex pattern.

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ const clearButton = document.getElementById("clear");
 const output = document.getElementById("output");
 let isError = false;
 const cleanInputString = (str) => {
-    const regex = /[+-\s]/;
+    const regex = /[+-\s]/g;
 };
 cleanInputString("huio677+ 777klk");
